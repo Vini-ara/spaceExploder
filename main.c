2396ac91c2201250314b683bd71b1b9b95e3c53f
Vinicius Araujo <vinicius.ar02@gmail.com>
2022-07-01 16:03:47 -0300

    meteor projective colision initially added

--- main.c
+++ main.c
@@ -9,18 +9,59 @@ typedef struct _Position {
   int x, y;
 } Position;
 
+/* Global Variabales */
 const int METEOR_WIDTH = 10, METEOR_HEIGHT = 4, SHIP_WIDTH = 12, SHIP_HEIGHT = 3;
 
-int IS_GAME_RUNNING = 1, FRAMES = 0, MAXY, MAXX, METEOR_QNT, SPAWN_CHANCE = 5;
+int IS_GAME_RUNNING = 1, FRAMES = 0, MAXY, MAXX, METEOR_QNT, SPAWN_CHANCE = 5, FIRE_RATE = 10;
 
-Position SHIP, METEORS[30];
+Position SHIP, METEORS[30], PROJECTILES[100];
 
+/*
+ * generates a new projectile at the first free position of the projectiles vector
+ */
+void fire_projectile() {
+  int starting_pos_y = SHIP.y + 2; // center of the ship
+  int starting_pos_x = SHIP.x + SHIP_WIDTH;
+
+  for(int i = 0; i < 100; ++i) {
+    if(PROJECTILES[i].x == -1) {
+      PROJECTILES[i].x = starting_pos_x;
+      PROJECTILES[i].y = starting_pos_y;
+      break;
+    }
+  }
+}
+
+void check_projectile_colision(int x, int y, int index) {
+  for(int i = 0; i < 100; ++i) {
+    int projectile_x = PROJECTILES[i].x;
+    int projectile_y = PROJECTILES[i].y;
+
+    if(projectile_x >= x && projectile_y >= y) {
+      PROJECTILES[i].x = -1;
+      METEORS[index].x = -1;
+      break;
+    }
+  }
+}
+
+void print_projectile(int x, int y) {
+  attroff(COLOR_PAIR(1));
+  
+  attron(COLOR_PAIR(2));
+  mvaddch(y, x, '0');
+  attroff(COLOR_PAIR(2));
+
+  attron(COLOR_PAIR(1));
+}
+
+/*
+ * uses Rand function to determinate if a meteor should be printed and in wihch position
+ */
 void generate_random_meteor() {
   int random = (int) (((double) rand() / RAND_MAX) * 100.0);  
 
   if(random <= SPAWN_CHANCE) {
-    METEOR_QNT++;
-
     for(int i = 0; i < 30; ++i) {
       if(METEORS[i].x == -1) {
         METEORS[i].x = MAXX - METEOR_WIDTH;
@@ -49,12 +90,18 @@ int check_superior_colision(int y)
   return 0;
 }
 
-void check_colision(Position meteor) 
+/*
+ * Checks whether the ship is coliding with the meteor passed as argument
+ */
+void check_ship_colision(Position meteor) 
 {
   if(meteor.x <= SHIP.x + SHIP_WIDTH && (check_inferior_colision(meteor.y) || check_superior_colision(meteor.y)))
     IS_GAME_RUNNING = 0;
 }
 
+/* 
+ * Process all the key inputs done during game
+ */
 void handle_input(int ch) 
 {
   switch (ch) {
@@ -73,6 +120,9 @@ void handle_input(int ch)
   }   
 }
 
+/*
+ * Spaceship printing function
+ */
 void print_spaceship() 
 {
   mvprintw(SHIP.y, SHIP.x,     "   __");
@@ -81,6 +131,9 @@ void print_spaceship()
   mvprintw(SHIP.y + 3, SHIP.x, "   /_/  ");
 }
 
+/*
+ * Meteor printing function 
+ */
 void print_meteor(int x, int y) 
 {
   mvprintw(y, x,     "   ____");
@@ -90,6 +143,9 @@ void print_meteor(int x, int y)
   mvprintw(y + 4, x, " \\_o__ /");
 }
 
+/*
+ * home Screen Printing Function 
+ */
 void print_splash_screen(int x, int y) 
 {
   int printStartY = (int) (y / 2) - 6;
@@ -107,9 +163,9 @@ void print_splash_screen(int x, int y)
   mvprintw(printStartY + 9, printStartX,  "          | $$                                                      | $$                                                  ");
   mvprintw(printStartY + 10, printStartX, "          |__/                                                      |__/                                                  ");
 
-  attron(A_UNDERLINE);
+  attron(A_UNDERLINE | A_BLINK);
   mvprintw(printStartY + 13, (x / 2) - 5, "Press ENTER");
-  attroff(A_UNDERLINE);
+  attroff(A_UNDERLINE | A_BLINK);
 
   int ch = getch();
 
@@ -121,21 +177,22 @@ void print_splash_screen(int x, int y)
   }
 }
 
+/* 
+ * Intialization deatails referent to the ncurses library, and setting some basic game 
+ * logic intial settings
+*/
 void init() 
 {
   initscr();
 
+  srand(time(NULL));
+
   raw();
-  halfdelay(2);
-
-  keypad(stdscr, true);
-
   noecho();
 
-  getmaxyx(stdscr, MAXY, MAXX);
+  halfdelay(1);
 
-  SHIP.y = MAXY / 2;
-  SHIP.x = 2;
+  keypad(stdscr, true);
 
   if(has_colors() == FALSE)
   {	
@@ -143,28 +200,31 @@ void init()
     printf("Your terminal does not support color\n");
     exit(1);
   }
-
   start_color();
-
   init_pair(1, COLOR_WHITE, COLOR_BLACK);
+  init_pair(2, COLOR_RED, COLOR_BLACK);
+  attron(COLOR_PAIR(1));
+
+  getmaxyx(stdscr, MAXY, MAXX);
 
   curs_set(0);
 
+  SHIP.y = MAXY / 2;
+  SHIP.x = 2;
+
+  // sets a initial value to the x of the position arrays
   for(int i = 0; i < 30; ++i) 
     METEORS[i].x = -1;
+
+  for(int i = 0; i < 100; ++i)
+    PROJECTILES[i].x = -1;
 }
 
 int main() 
 {
   init();
 
-  attron(COLOR_PAIR(1) | A_BLINK);
-
-  srand(time(NULL));
-
   print_splash_screen(MAXX, MAXY);
-
-  METEOR_QNT = 0;
 
   while(IS_GAME_RUNNING) 
   {
@@ -172,16 +232,27 @@ int main()
 
     generate_random_meteor();
 
-    mvprintw(0, 0, "%d", FRAMES);
 
     for(int i = 0; i < 30; ++i) {
       if(METEORS[i].x != -1) {
         print_meteor(METEORS[i].x--, METEORS[i].y);
 
-        check_colision(METEORS[i]);
+        check_ship_colision(METEORS[i]);
+        check_projectile_colision(METEORS[i].x, METEORS[i].y, i);
       }
 
       if(METEORS[i].x == 0) METEORS[i].x = -1;
+    }
+
+    if(!(FRAMES % 30)) fire_projectile();
+
+    for(int i = 0; i < 100; ++i) {
+      if(PROJECTILES[i].x != -1) {
+        print_projectile(PROJECTILES[i].x++, PROJECTILES[i].y);
+
+      }
+
+      if(PROJECTILES[i].x == MAXX) PROJECTILES[i].x = -1;
     }
 
     FRAMES++;
